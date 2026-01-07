8423e42ff5b9d976f4abbcd6efaf53f135e395d7
celsojutcel-eng <celso.jutcel@gmail.com>
2026-01-07 14:00:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,19 +7,19 @@ const ASSETS = [
   './icons/icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
